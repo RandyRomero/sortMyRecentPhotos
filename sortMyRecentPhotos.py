81d3c0a847c32973379b4813f7538dd5aade0a07
RandyRomero <a.ya.miheev@gmail.com>
2017-01-15 16:51:47 +0300

    amend appearence of question 'Start to look for your photos?' // print how many rhere are unsorted files

--- sortMyRecentPhotos.py
+++ sortMyRecentPhotos.py
@@ -3,7 +3,7 @@
 import os, shutil, sys
 
 
-logFile = open('D:\\PythonPhoto\\sortedPhoto\\logFile.txt', w)
+logFile = open('D:\\PythonPhoto\\sortedPhoto\\logFile.txt', 'w')
 logFile.write('Program started. Log file created\n')
 
 
@@ -11,14 +11,17 @@ unsortedPhotos = ('D:\\PythonPhoto\\unsortedPhoto')
 sortedPhotos = ('D:\\PythonPhoto\\sortedPhoto')
 logFile.write('Path to main folders created\n')
 
-start = input('\nStart to look for your photos? (y/n)')
-logFile.write('Start to look for your photos? (y/n)\n')
+def sortEngine():
+	files = os.listdir(unsortedPhotos)
+	print('Here are ' + str(len(files)) + ' files.')
 
+start = input('\nStart to look for your photos? (y/n) Your answer is: ')
+logFile.write('Start to look for your photos? (y/n)\n')
 
 while True:
 	if start == 'y':
 		logFile.write('Got "y".\n')
-		#start sort engine
+		sortEngine()
 		break
 	elif start == 'n':
 		logFile.write('Got "n". Exit script.\n')
@@ -28,6 +31,8 @@ while True:
 		logFile.write('Got wrong input. Ask again...\n')
 		print('Input error. You should type in y or n')	
 		continue
+
+
 		
 
 
